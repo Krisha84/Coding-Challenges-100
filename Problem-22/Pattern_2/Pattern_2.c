b6384e3f7d1ded609376a027a5236f0408b161c0
Krisha Busa <23010101039@darshan.ac.in>
2025-08-30 19:16:05 +0530

    Add files via upload

---
+++ Problem-22/Pattern_2/Pattern_2.c
@@ -0,0 +1,23 @@
+// *********
+//  *******
+//   *****
+//    ***
+//     *
+
+#include<stdio.h>
+
+void main(){
+
+    for(int i = 5; i >= 1; i--){
+        for(int j = 1; j <= 5 - i; j++){
+            printf(" ");
+        }
+
+        for(int j = 1; j <= 2*i - 1; j++){
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+}
