82c52bc75438e19b1f8bc8bf94ecaee24983179d
Krisha Busa <23010101039@darshan.ac.in>
2025-08-30 19:33:15 +0530

    Add file via upload

--- Problem-22/Pattern_2/Pattern_2.py
+++ Problem-22/Pattern_2/Pattern_2.py
@@ -4,5 +4,11 @@
 #    ***
 #     *
 
+# for i in range(5, 0, -1) :
+#     print(" " * (5 - i) + "*" * (2*i - 1))
+
 for i in range(5, 0, -1) :
-    print(" " * (5 - i) + "*" * (2*i - 1))
+    space = " " * (5 - i)        
+    star = "*" * (2 * i - 1)   
+
+    print(space + star)
